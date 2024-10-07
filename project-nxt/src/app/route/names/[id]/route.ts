52e3e03cd730b8e4a842e8d70ffbc28a3db09b5c
Jussara Andrade <jussara.jandrade1993@outlook.com>
2024-10-06 21:00:00 -0300

    feat: settings url query, redirect, cookies and cache

--- project-nxt/src/app/route/names/[id]/route.ts
+++ project-nxt/src/app/route/names/[id]/route.ts
@@ -1,11 +1,14 @@
+import { redirect } from "next/navigation";
 import { data } from "../data";
 
 export async function GET(
     _request: Request,
     { params }: {params: { id: string } }
 ) {
+    if(parseInt(params.id) > data.length) {
+        redirect('/route/names')
+    }
     const name = data.find(name => name.id == parseInt(params.id))
-
     return Response.json(name)
 }
 
--- project-nxt/src/app/route/profile/api/route.ts
+++ project-nxt/src/app/route/profile/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-    return new Response('Profile Route')
+import { NextRequest } from "next/server";
+import { headers, cookies } from 'next/headers'
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers()
+
+    cookies().set('token', '1234abc')
+    const theme = request.cookies.get('theme')
+     
+    console.log(requestHeaders.get('Authorization'))
+    console.log(headerList.get('Authorization'))
+
+    console.log(theme)
+    console.log(cookies().get('token'))
+
+    return new Response('<h1>Profile Route</h1>', {
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
+        }
+    })
 }
